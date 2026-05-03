f484d440b6d8d08438308e10f049ccbc3ac2c18d
ujjwal781 <sinhaujjwal781@gmail.com>
2026-05-03 13:14:38 +0530

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = "https://hvvrvskeyxwkxkrpwvcj.supabase.co";
+const SUPABASE_KEY = "sb_publishable_PKBS-MPbyCMHKSRBxUN8xw_nGNfqrBx";
+
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+window.SUPABASE_KEY = SUPABASE_KEY;
