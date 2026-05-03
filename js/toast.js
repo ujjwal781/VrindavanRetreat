1d7d6c48c70eb3684e4567da891308a681168805
ujjwal781 <sinhaujjwal781@gmail.com>
2026-05-03 13:14:16 +0530

    Create toast.js

---
+++ js/toast.js
@@ -0,0 +1,18 @@
+function showToast(message, type = "success") {
+  const container = document.getElementById("toastContainer");
+
+  if (!container) {
+    alert(message);
+    return;
+  }
+
+  const toast = document.createElement("div");
+  toast.className = `toast ${type}`;
+  toast.textContent = message;
+
+  container.appendChild(toast);
+
+  setTimeout(() => {
+    toast.remove();
+  }, 2000);
+}
